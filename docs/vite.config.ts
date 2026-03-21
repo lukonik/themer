37a84593d316f1cf8ee5a43b2030158af99f22d8
luka <lukaonik@gmail.com>
2026-03-21 13:12:55 +0400

    docs: move to new prestige

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -8,67 +8,15 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { ohImage } from "@lonik/oh-image/plugin";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 const config = defineConfig({
   base: "/themer/",
   plugins: [
     ohImage({
-      outDir: ".output/public",
+      outDir: "dist/client",
     }),
-    prestige({
-      title: "Themer",
-      collections: [
-        {
-          id: "docs",
-          items: [
-            {
-              label: "Get Started",
-              slug: "docs/get-started",
-            },
-            {
-              label: "Hydration",
-              slug: "docs/hydration",
-            },
-            {
-              label: "References",
-              items: [
-                {
-                  label: "ThemeProvider Reference",
-                  slug: "docs/theme-provider-reference",
-                },
-                {
-                  label: "useTheme Reference",
-                  slug: "docs/use-theme-reference",
-                },
-              ],
-            },
-            {
-              label: "Guides",
-              items: [
-                {
-                  label: "Tailwind",
-                  slug: "docs/tailwind",
-                },
-                {
-                  label: "Multi Theming",
-                  slug: "docs/multi-theming",
-                },
-                {
-                  label: "Storage",
-                  slug: "docs/storage",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    }),
+    prestige(),
     devtools(),
-    nitro({
-      rollupConfig: { external: [/^@sentry\//] },
-      renderer: false,
-      baseURL: "/themer",
-    }),
+
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart({
